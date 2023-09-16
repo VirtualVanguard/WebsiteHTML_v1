8d1cd9d4ef3fcaaa3193c33e06d7583c12714e3a
Hugo Doueil <46538211+Ormidales@users.noreply.github.com>
2023-09-17 00:13:55 +0200

    Add files via upload

--- ressources/javascript/header.js
+++ ressources/javascript/header.js
@@ -1,17 +1,37 @@
+// Constantes pour les classes CSS
+const HEADER_CLASS_HIDDEN = 'hidden';
+const HEADER_CLASS_FIXED = 'fixed';
+
 // Sélectionnez l'élément du header
 const header = document.querySelector('.sticky-header');
 
-// Écoutez l'événement de défilement de la page
-window.addEventListener('scroll', () => {
-  // Obtenez la position verticale actuelle de la page
+// Fonction pour faire apparaître la navbar avec un fondu
+function fadeInNavbar() {
+  header.style.opacity = '1'; // Définir l'opacité à 1 pour la faire apparaître
+}
+
+// Fonction pour faire disparaître la navbar avec un fondu
+function fadeOutNavbar() {
+  header.style.opacity = '0'; // Définir l'opacité à 0 pour la faire disparaître
+}
+
+// Fonction pour mettre à jour la classe du header en fonction de la position de défilement
+function updateHeaderClass() {
   const scrollY = window.scrollY;
 
-  // Mettez à jour la classe CSS en fonction de la position de défilement
-  if (scrollY > 100) {
-    header.classList.remove('hidden');
-    header.classList.add('fixed');
+  if (scrollY > 0) {
+    fadeInNavbar();
+    header.classList.remove(HEADER_CLASS_HIDDEN);
+    header.classList.add(HEADER_CLASS_FIXED);
   } else {
-    header.classList.remove('fixed');
-    header.classList.add('hidden');
+    fadeOutNavbar();
+    header.classList.remove(HEADER_CLASS_FIXED);
+    header.classList.add(HEADER_CLASS_HIDDEN);
   }
-});
+}
+
+// Écoutez l'événement de défilement de la page
+window.addEventListener('scroll', updateHeaderClass);
+
+// Documentation du code
+console.log("virtualvanguard_javascript_header : Code initialisé");
--- ressources/javascript/parallax.js
+++ ressources/javascript/parallax.js
@@ -1,6 +1,20 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        var scrollTop = $(this).scrollTop();
-        $('.parallax-bg').css('background-position-y', -(scrollTop / 2) + 'px');
-    });
+// Créez une fonction réutilisable pour gérer l'effet de parallaxe
+function handleParallax() {
+    const scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    const canvas = document.getElementById("parallax-bg");
+
+    if (canvas) {
+        canvas.style.backgroundPositionY = (scrolled / 10) + "px";
+    }
+}
+
+// Ajoutez un gestionnaire d'événement de défilement
+window.addEventListener("scroll", function () {
+    requestAnimationFrame(handleParallax);
 });
+
+// Exécutez la fonction une fois au chargement de la page pour une position initiale correcte
+window.addEventListener("load", handleParallax);
+
+// Documentation du code
+console.log("virtualvanguard_javascript_parallax : Code initialisé");
